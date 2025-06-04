7fd7a552f6a733800f8c088ad3f770924942f3a0
ilham.morsalin <ilhammorsalin@gmail.com>
2025-06-04 21:14:13 +0600

    Refactor Header component and update index.html for image path correction; add .editorconfig and .prettierrc for consistent coding styles

--- my-portfolio/src/components/Header.jsx
+++ my-portfolio/src/components/Header.jsx
@@ -1,15 +1,28 @@
 function Header() {
   return (
     <header className="bg-white shadow p-6 flex justify-between items-center">
+      {" "}
       <div className="flex items-center">
-        <img src="/elamDP.png" alt="elamDP icon" className="w-10 h-10 mr-3" style={{imageRendering: 'auto'}} />
+        <img
+          src="./public/elamDP.png"
+          alt="elamDP icon"
+          className="w-10 h-10 mr-3"
+        />
         <h1 className="text-2xl font-bold text-gray-800">Ilham's Portfolio</h1>
       </div>
       <nav className="space-x-6">
-        <a href="#home" className="text-gray-600 hover:text-blue-500">Home</a>
-        <a href="#about" className="text-gray-600 hover:text-blue-500">About</a>
-        <a href="#projects" className="text-gray-600 hover:text-blue-500">Projects</a>
-        <a href="#contact" className="text-gray-600 hover:text-blue-500">Contact</a>
+        <a href="#home" className="text-gray-600 hover:text-blue-500">
+          Home
+        </a>
+        <a href="#about" className="text-gray-600 hover:text-blue-500">
+          About
+        </a>
+        <a href="#projects" className="text-gray-600 hover:text-blue-500">
+          Projects
+        </a>
+        <a href="#contact" className="text-gray-600 hover:text-blue-500">
+          Contact
+        </a>
       </nav>
     </header>
   );
