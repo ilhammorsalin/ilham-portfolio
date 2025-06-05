6df4ab8ab0413f925eadc8d39b9fe29bf1ab83be
ilham.morsalin <ilhammorsalin@gmail.com>
2025-06-05 10:19:46 +0600

    Fix image paths in index.html and Header component; add elamDP.png asset

--- my-portfolio/src/components/Header.jsx
+++ my-portfolio/src/components/Header.jsx
@@ -1,13 +1,10 @@
+import elamDP from "../assets/elamDP.png";
+
 function Header() {
   return (
     <header className="bg-white shadow p-6 flex justify-between items-center">
-      {" "}
       <div className="flex items-center">
-        <img
-          src="./public/elamDP.png"
-          alt="elamDP icon"
-          className="w-10 h-10 mr-3"
-        />
+        <img className="w-20 h-20 mr-3" src={elamDP} alt="elam dp" />
         <h1 className="text-2xl font-bold text-gray-800">Ilham's Portfolio</h1>
       </div>
       <nav className="space-x-6">
