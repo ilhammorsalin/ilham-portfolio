bd575d04a22bd16653c30c1749c001e608270f8d
ilham.morsalin <ilhammorsalin@gmail.com>
2025-06-11 19:56:35 +0600

    removed all the ai stuff and starting fresh

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,23 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header.jsx";
-import Main from "./main.jsx";
-import Footer from "./components/Footer.jsx";
-
 function App() {
-  return (
-    <Router>
-      <div className="min-h-screen bg-gray-50 font-mono">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/about" element={<Main />} />
-          <Route path="/projects" element={<Main />} />
-          <Route path="/contact" element={<Main />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
-  );
+
 }
 
 export default App;
--- my-portfolio/src/main.jsx
+++ my-portfolio/src/main.jsx
@@ -1,32 +1,5 @@
-import { useLocation } from "react-router-dom";
-import HomeSection from "./sections/HomeSection";
-import AboutSection from "./sections/AboutSection";
-import ProjectsSection from "./sections/ProjectsSection";
-import ContactSection from "./sections/ContactSection";
-
 function Main() {
-  const location = useLocation();
-
-  const renderSection = () => {
-    switch (location.pathname) {
-      case "/":
-        return <HomeSection />;
-      case "/about":
-        return <AboutSection />;
-      case "/projects":
-        return <ProjectsSection />;
-      case "/contact":
-        return <ContactSection />;
-      default:
-        return <HomeSection />;
-    }
-  };
-
-  return (
-    <main className="p-8 max-w-3xl mx-auto">
-      {renderSection()}
-    </main>
-  );
+ 
 }
 
 export default Main;
