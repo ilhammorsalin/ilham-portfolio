1dbb82d2a468ad9e5d52ad86276182be93300cd8
ilham.morsalin <ilhammorsalin@gmail.com>
2025-06-21 01:55:30 +0600

    Refactor App component to remove Card instances and update background color in CSS to white

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,23 +1,10 @@
 import { Card } from "./components/Card";
-import elamDP from "./assets/elamDP.png";
-import sun from "./assets/sun scarecrow square.png";
-import pulp from "./assets/pulp gif.gif";
 
 function App() {
   return (
     <div
-      className="min-h-screen bg-gray-800 m-10 p-5 
-      flex flex-row flex-wrap justify-evenly content-start">
-      <Card image={elamDP} header="elam" text="lazy ass dude" btnText="Text" />
-
-      <Card
-        image={sun}
-        header="naturelover55"
-        text="watch a sunset with me"
-        btnText="Text"
-      />
-      <Card image={pulp} header="s.jackson" text="that mf" btnText="Text" />
-    </div>
+      className="min-h-screen bg-green-400 p-5
+      flex flex-row flex-wrap justify-evenly content-start"></div>
   );
 }
 
