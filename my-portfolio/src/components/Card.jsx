3ba19634a902a775f9a75e23d44ca76c1411e9be
ilham.morsalin <ilhammorsalin@gmail.com>
2025-09-24 09:01:16 +0600

    setup default state and clean start

---
+++ my-portfolio/src/components/Card.jsx
@@ -0,0 +1,13 @@
+
+// Create your Card component here
+// Example: 
+// const Card = ({ title, description }) => {
+//   return (
+//     <div>
+//       <h3>{title}</h3>
+//       <p>{description}</p>
+//     </div>
+//   )
+// }
+// 
+// export default Card
